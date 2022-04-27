7c2beed56c051aec8821d9c5ed5fd0a9d6261e7b
Jacob Waters <fierywaters13@gmail.com>
2022-04-27 01:48:50 -0700

    cleaned up a tad

--- setupSshServer.sh
+++ setupSshServer.sh
@@ -12,30 +12,26 @@ read remoteUser
 echo "Enter the IP Address for the remote host"
 read remoteIpAddress
 
-
+#send public key to remote host's authorized_keys
 cat $HOME/.ssh/$keyName.pub | ssh $remoteUser@$remoteIpAddress "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
 
+#user input to determine port
 question="What port would you configure the remote server to use? "
 verify="" #loop will run at least once
 while [[ $verify != 'yes' ]] && [[ $verify != 'y' ]] # While input isn't correct ask again
 do
     read -p "$question" remotePort # Ask the user to enter a string
     read -p "Did you type it right? [yes, y] to continue " verify
-    echo $verify
 done
 
 
 
 echo "disabling password login"
 echo "Setting remote port to $remotePort"
-
-
-
 #-t option uses tty which allows sudo, "| sudo tee -a" substitutes ">>" as the latter can't be used with sudo
-ssh -t $remoteUser@$remoteIpAddress "echo PasswordAuthentication no | sudo tee -a /etc/ssh/sshd_config && echo Port $remotePort | sudo tee -a /etc/ssh/sshd_config 
-&& echo ChallengeResponseAuthentication no | sudo tee -a /etc/ssh/sshd_config && sudo systemctl restart sshd"
+ssh -t $remoteUser@$remoteIpAddress "echo PasswordAuthentication no | sudo tee -a /etc/ssh/sshd_config && echo Port $remotePort | sudo tee -a /etc/ssh/sshd_config && echo ChallengeResponseAuthentication no | sudo tee -a /etc/ssh/sshd_config && sudo systemctl restart sshd"
 
-echo the below ssh login attempts will fail if password login is disabled
+echo "The below ssh login attempts will fail if password login is disabled"
 ssh $remoteUser@$remoteIpAddress "echo WARNING the script faile to disable password login"
 ssh -p $remotePort $remoteUser@$remoteIpAddress "echo WARNING the script failed to disable password login"
 
@@ -50,5 +46,5 @@ echo "\tIdentityFile $identityFileLocation" >> $configFile
 echo "\tUser $remoteUser" >> $configFile
 echo "" >> $configFile #empty space
 
+echo "If connection below succeeds, ssh key authentication was successful"
 ssh $keyName "echo ssh key authentication is successful"
-
